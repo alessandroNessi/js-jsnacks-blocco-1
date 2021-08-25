11ea9917620d8c120b63bd9b7078e2b2fb3782e2
alessandroNessi <alessandronessi@msn.com>
2021-08-25 17:02:23 +0200

    added interface and media query

--- snack-5/script/script.js
+++ snack-5/script/script.js
@@ -1,9 +1,12 @@
-var array=[];
-var userInput;
-for(var i=0; i<6; i++){
-    userInput=prompt("inserisci un numero");
-    if(userInput%2!=0){
-        array.push(userInput);
+function calculate(){
+    var array=[];
+    var userInput;
+    document.getElementById("results__output").innerHTML ="";
+    for(var i=0; i<document.getElementsByClassName("input-group-sm").length; i++){
+        userInput=parseInt(document.getElementById("inputGroup"+ (i+1)).value);
+        if(!(isNaN(userInput)) && userInput%2!=0){
+            array.push(userInput);
+            document.getElementById("results__output").innerHTML += userInput;
+        }
     }
 }
-console.log(array);
